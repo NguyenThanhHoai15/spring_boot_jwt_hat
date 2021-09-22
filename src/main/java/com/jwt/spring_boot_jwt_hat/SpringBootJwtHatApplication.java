c54aba4e9ca02318f61c8617c91418f10f8e7c89
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-09-22 10:24:16 +0700

    git demo

---
+++ src/main/java/com/jwt/spring_boot_jwt_hat/SpringBootJwtHatApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.spring_boot_jwt_hat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtHatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtHatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jwt/spring_boot_jwt_hat/SpringBootJwtHatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.spring_boot_jwt_hat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtHatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
